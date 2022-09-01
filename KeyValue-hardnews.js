524eeff3309f78938664e3fa9569c36973013566
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-09-01 17:44:48 -0300

    Create KeyValue-hardnews.js

---
+++ KeyValue-hardnews.js
@@ -0,0 +1,13 @@
+function rosValidation() {
+    var script = document.createElement('script');
+    script.src = 'https://pkioshima-recordtv.github.io/keyvalue-hardnews/ros_validation.js';
+    script.async = true;
+    document.body.appendChild(script);
+}
+
+function condition() {
+    var script = document.createElement('script');
+    script.src = 'https://pkioshima-recordtv.github.io/keyvalue-hardnews/parceiros_condition.js';
+    script.async = true;
+    document.body.appendChild(script);
+}
